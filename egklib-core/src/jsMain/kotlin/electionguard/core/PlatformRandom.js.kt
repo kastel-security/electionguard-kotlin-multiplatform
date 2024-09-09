1c6958ff6f16c261db100659736ad38e3d9660bd
lorenzHeinrich <lorenz.heinrich@student.kit.edu>
2024-09-09 03:29:18 +0200

    adjust PlatformRandom.js.kt to be compatible with esModules

--- egklib-core/src/jsMain/kotlin/electionguard/core/PlatformRandom.js.kt
+++ egklib-core/src/jsMain/kotlin/electionguard/core/PlatformRandom.js.kt
@@ -11,19 +11,17 @@ private external interface Window {
     val crypto: Crypto
 }
 
+@JsModule("crypto") // node:crypto
+external val crypto: dynamic
+
 private external val window: Window
-
-private external fun require(module: String): dynamic
-
-fun requireNode(module: String): dynamic = require(module)
 
 /** Get "secure" random bytes from the native platform */
 // this uses the Web Crypto API both in the browser and in Node.js
 actual fun randomBytes(length: Int) =
     when (getPlatform()) {
         Platform.BROWSER -> window.crypto.getRandomValues(ByteArray(length))
-        Platform.NODE -> requireNode("crypto")
-            .getRandomValues(ByteArray(length)).unsafeCast<ByteArray>()
+        Platform.NODE -> crypto.randomBytes(length)
     }
 
 fun getPlatform(): Platform =
